f27df9f70d74bacb9ad1be783e51d2829c897489
Ben Boeckel <ben.boeckel@kitware.com>
2014-02-23 13:50:44 -0500

    Remove ENV tests for CMP0052
    Parentheses are now allowed in variable names.

--- Tests/RunCMake/CMP0052/CMP0052-ENV-NEW.cmake
+++
@@ -1,4 +0,0 @@
-cmake_policy(SET CMP0052 NEW)
-
-set("ENV{e(x)}" value)
-message("-->$ENV{e(x)}<--")
--- Tests/RunCMake/CMP0052/CMP0052-ENV-OLD.cmake
+++
@@ -1,4 +0,0 @@
-cmake_policy(SET CMP0052 OLD)
-
-set("ENV{e(x)}" value)
-message("-->$ENV{e(x)}<--")
